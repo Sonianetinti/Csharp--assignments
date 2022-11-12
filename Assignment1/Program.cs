6502876945d73125bd2f798aa8d38cbc799eed94
Sonianetinti <115173521+Sonianetinti@users.noreply.github.com>
2022-11-12 05:45:40 +0000

    Add files via upload

---
+++ Assignment1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second number");
+            int j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Addition:" + (i + j));
+            Console.WriteLine("Subtraction:" + (i - j));
+            Console.WriteLine("division:" + (i / j));
+            Console.WriteLine("Mulltiplication:" + (i * j));
+            Console.ReadKey();
+        }
+
+    }
+}
