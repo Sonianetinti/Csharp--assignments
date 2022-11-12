9b1e751ad50c132089fa41e2fbfb0db3070ddb53
Sonianetinti <115173521+Sonianetinti@users.noreply.github.com>
2022-11-12 05:52:03 +0000

    Add files via upload

---
+++ Assignment7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace assignment7 
+{ class directory 
+    { 
+        static void Main(string[] args) 
+       { 
+         string path; 
+         string root = @"C:\newtext"; 
+         string subdir = @"C:\newtext\Sonia"; 
+            Directory.CreateDirectory(root); 
+            Directory.CreateDirectory(subdir); 
+            path = "C:\\newtext\\Sonia"; 
+            Console.WriteLine(path);
+            Console.ReadKey();
+        } 
+    } 
+}
